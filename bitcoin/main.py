74574f0ea3f516eda72498ddf518e748a45914cd
Pierre DEPRETZ <pdepretz59@gmail.com>
2020-05-04 18:29:18 +1000

    bitcoin

--- bitcoin/main.py
+++ bitcoin/main.py
@@ -2,7 +2,7 @@ import requests
 from bs4 import BeautifulSoup
 
 def get_base_url(article=None):
-    url = 'https://www.coindesk.com/'
+    url = 'https://www.coindesk.com'
     if article is not None:
         url += article
     return url
@@ -10,12 +10,21 @@ def get_base_url(article=None):
 def get_text(article=None):
     if article is not None:
         soup = BeautifulSoup(requests.get(get_base_url(article)).content, 'lxml')
-        return ''.join([ item.text for item in soup.select('div.article-pharagraph') ])
+        return ''.join([ item.text for item in soup.find_all('div','article-pharagraph') ])
     else:
         print(' Error: no article provided\nFormat: get_text(<article>)')
         return None
 
+def get_articles():
+    articles = []
+    soup = BeautifulSoup(requests.get(get_base_url('/news')).content, 'lxml')
+    div = soup.find_all('div','text-content')
+    for item in div:
+        list = [ item for item in item.find_all('a',href=True) ]
+        articles += [ item['href'].strip() for item in list if item['href'].count('/') == 1 ]
+    return set(articles)
+        
 if __name__ == "__main__":
-    #Exemple local, sinon passer l'article en argument de la fonction get_text(<article>)
-    article = 'bitcoin-ekes-out-gains-but-remains-in-red-amid-broader-market-rebound'
-    print(get_text(article))
+    for article in get_articles():
+        print(get_text(article))
+    print('-----\n {0} articles have been extracted \n-----'.format(len(get_articles())))
