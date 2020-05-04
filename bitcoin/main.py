dce4b610ee52afe4e274340f1fd884e6b9fa2705
Pierre DEPRETZ <pdepretz59@gmail.com>
2020-05-04 15:56:20 +1000

    update bitcoin

--- bitcoin/main.py
+++ bitcoin/main.py
@@ -12,7 +12,7 @@ def get_text(article=None):
         soup = BeautifulSoup(requests.get(get_base_url(article)).content, 'lxml')
         return ''.join([ item.text for item in soup.select('div.article-pharagraph') ])
     else:
-        print('  Error: no article provided\n Format: main.py <article>\nExemple: main.py bitcoin-ekes-out-gains-but-remains-in-red-amid-broader-market-rebound')
+        print('  Error: no article provided\n Format: get_text(<article>)\nExemple: get_text(bitcoin-ekes-out-gains-but-remains-in-red-amid-broader-market-rebound)')
 
 if __name__ == "__main__":
     #Exemple local, sinon passer l'article en parametre de la fonction get_text(<article>)
